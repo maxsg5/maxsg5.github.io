58b97581657c5c50df0286a3078be239fa27bf12
Max Schafer <maxsg5@hotmail.com>
2023-10-23 23:28:57 -0600

    HW

---
+++ Desn340-53site-redesign-oct18/scripts/pencil.js
@@ -0,0 +1,13 @@
+var icons = document.getElementById("FeatureIcons");
+var SurfacePressure = document.getElementById("SurfacePressure");
+var Erase = document.getElementById("Erase");
+var PalmRejection = document.getElementById("PalmRejection");
+
+SurfacePressure.onmouseover = function () {
+    icons.style.backgroundPositionY = "-194px";
+}
+
+Erase.onmouseover = function () {
+    icons.style.backgroundPositionY = "-390px";
+}
+
