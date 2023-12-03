4578f880cb23a20c9cc41ce0e9fdc9c3d7c33a10
Max Schafer <maxsg5@hotmail.com>
2023-12-03 16:35:59 -0700

    Phone

--- Ptarmigan/scripts/script.js
+++ Ptarmigan/scripts/script.js
@@ -1,4 +1,10 @@
 // On click event for hiding the mobile menu when a link is clicked.
 $('.nav-link').on('click', function () {
     $('#offcanvasNavbar').offcanvas('hide');
-  });
+});
+  
+
+$('.call-box').on('click', function() {
+  var phoneNumber = $('.phone-number').text();
+  window.location.href = "tel:" + phoneNumber;
+});
